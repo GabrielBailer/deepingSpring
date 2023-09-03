382fbd03a371dd4583c3318a6ffe8395145d785e
GabrielBailer <gabrielbailer80@gmail.com>
2023-09-02 23:35:13 -0300

    feat: first config

---
+++ src/main/java/com/deepeningInSpringBoot/DeepeningInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.deepeningInSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepeningInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepeningInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepeningInSpringBoot/DeepeningInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepeningInSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepeningInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
