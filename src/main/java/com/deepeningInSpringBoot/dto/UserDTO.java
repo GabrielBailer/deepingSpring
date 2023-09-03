8b709fb9f4183e454f11c2c2db78485ad1c8b270
GabrielBailer <gabrielbailer80@gmail.com>
2023-09-03 18:01:04 -0300

    feat: create UserDTO

---
+++ src/main/java/com/deepeningInSpringBoot/dto/UserDTO.java
@@ -0,0 +1,8 @@
+package com.deepeningInSpringBoot.dto;
+
+import com.deepeningInSpringBoot.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String name, String lastName, String document, BigDecimal balance, String email, String passowrd, UserType userType) {
+}
