47faa070e6abe2a659c52deaa115366d7b54eb33
GabrielBailer <gabrielbailer80@gmail.com>
2023-09-02 23:54:06 -0300

    feat: create Transaction Repository

---
+++ src/main/java/com/deepeningInSpringBoot/domain/repositories/TransitionRep.java
@@ -0,0 +1,6 @@
+package com.deepeningInSpringBoot.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransitionRep extends JpaRepository<TransitionRep, Long> {
+}
